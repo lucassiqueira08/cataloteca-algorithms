140c7518d5f5b728d37e23c3ca1b865af245407a
Lucas Siqueira <lalvessiqueira8@gmail.com>
2019-08-30 23:18:05 -0300

    cria exemplos de comparacao de strings com o leveinstein

--- app/LeveinsteinDistance.cs
+++ app/LeveinsteinDistance.cs
@@ -7,7 +7,11 @@ public class LevenshteinDistance {
     public static Dictionary<string, int> Entrypoint(string string1, string string2) {
 
         var distance = string1.LevenshteinDistance(string2);
+        // TODO: Melhorar lógica da similaridade, quando a similaridade é muito baixa é atribuido zero.
         var similarity = 100 - ((distance*100) / string1.Length);
+        if (similarity < 0){
+             similarity = 0;
+        }
 
         var dictionary = new Dictionary<string, int>() {
              {"distance", distance},
--- app/Program.cs
+++ app/Program.cs
@@ -9,16 +9,39 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            System.Console.Write("Por favor insira a base: ");
+            string[] productsList;
+
+            System.Console.Write("Por favor insira uma palavra: ");
             string string1 = Console.ReadLine();
-            System.Console.Write("Por favor insira o alvo: ");
-            string string2 = System.Console.ReadLine();
 
-            var distanceDict = LevenshteinDistance.Entrypoint(string1, string2);
-            System.Console.WriteLine("A distância é de {0}", distanceDict["distance"]);
-            System.Console.WriteLine("A similaridade entre as strings é de {0}%", distanceDict["similarity"]);
+            productsList = new string[]{
+                "Abracadeira Regulavel Tipo Rosca Sem Fim Faixa Regulagem 14-22Mm Largura Fita 9Mm Espessura 0.7Mm Aco Sae 1020 Acabamento Galvanizado Eletrolitico",
+                "Abracadeira Regulavel Rosca Sem Fim Faixa Regulagem 19-38Mm Aco Inox Aisi 316",
+                "Abracadeira Regulavel Rosca Sem Fim Faixa Regulagem 25-38Mm Aco Sae 1020"
+            };
+
+            foreach(var p in productsList){
+                var distanceDict = LevenshteinDistance.Entrypoint(string1, p);
+                PrintMetrics(p, distanceDict["distance"], distanceDict["similarity"]);
+            }
 
 
+            
+           
+
+        }
+
+        public static void PrintMetrics(string product, int distance, int similarity)
+        {
+            System.Console.WriteLine(' ');
+            System.Console.WriteLine(' ');
+            System.Console.WriteLine("************************");
+            System.Console.WriteLine(product);
+            System.Console.WriteLine("************************");
+            System.Console.WriteLine("A distância é de {0}", distance);
+            System.Console.WriteLine("A similaridade entre as strings é de {0}%", similarity);
+            System.Console.WriteLine(' ');
+            System.Console.WriteLine(' ');
         }
     }
 }
