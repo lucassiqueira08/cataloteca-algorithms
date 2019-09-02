206c12b4e77ced20a32200d76c7a0eb9e8a82410
Lucas Siqueira <lucas.siqueira@skyone.solutions>
2019-09-02 14:46:52 -0300

    fix similarity logic

--- app/LeveinsteinDistance.cs
+++ app/LeveinsteinDistance.cs
@@ -3,22 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using DuoVia.FuzzyStrings;
 
-public class LevenshteinDistance {
-    public static Dictionary<string, int> Entrypoint(string string1, string string2) {
+public class LevenshteinDistance
+{
+    public static Dictionary<string, int> Entrypoint(string string1, string string2)
+    {
+        int divisor;
+        var dictionary = new Dictionary<string, int>();
+        if (string1.Length == 0 || string2.Length == 0)
+        {
+             dictionary.Add("distance", 100);
+             dictionary.Add("similarity", 0);
 
-        var distance = string1.LevenshteinDistance(string2);
-        // TODO: Melhorar lógica da similaridade, quando a similaridade é muito baixa é atribuido zero.
-        var similarity = 100 - ((distance*100) / string1.Length);
-        if (similarity < 0){
-             similarity = 0;
-        }
-
-        var dictionary = new Dictionary<string, int>() {
-             {"distance", distance},
-             {"similarity", similarity}
+            return dictionary;
         };
 
+        if (string1.Length > string2.Length)
+        {
+            divisor = string1.Length;
+        }
+        else
+        {
+            divisor = string2.Length;
+        };
+
+        var distance = string1.LevenshteinDistance(string2);
+        var similarity = 100 - ((distance * 100) / divisor);
+
+        dictionary.Add("distance", distance);
+        dictionary.Add("similarity", similarity);
         return dictionary;
-   } 
+    }
 
 }
