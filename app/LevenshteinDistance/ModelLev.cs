02bbf8a7f0b84a443591c21fd2f654086ea3360e
Lucas Siqueira <lalvessiqueira08@gmail.com>
2019-09-07 00:10:16 -0300

    override tostring method

--- app/LevenshteinDistance/ModelLev.cs
+++ app/LevenshteinDistance/ModelLev.cs
@@ -6,6 +6,11 @@ namespace level.app.LevenshteinDistance
     {
         public double distance { get; set; }
         public double similarity { get; set; }
+
+        public override string ToString()
+        {
+            return $"Distance: {distance:F2} | Similarity: {similarity:F2}%";
+        }
     }
 
 }
